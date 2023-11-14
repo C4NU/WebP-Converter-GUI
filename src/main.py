26383735075ea3dec4ec8fdfcdf70deceb2a307d
C4NU <canu1832@gmail.com>
2023-11-14 16:27:56 +0900

    [FIX] resources 폴더 연동 재작업 [TO FIX] webp 패키지 사라짐..?

---
+++ src/main.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Hyo Jae Jeon (CANU) canu1832@gmail.com
+
+########################################
+import subprocess
+########################################
+
+def main():
+    subprocess.run(['python3', '--version'], shell=False)
+    import app_window
+    print("App Window module Loaded")
+    app = app_window.WebpApp()
+    print("Webp App Module Loaded")
+    app.show()
+    app.exec()
+
+if __name__ == "__main__":
+    main()
